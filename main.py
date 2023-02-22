41fc3cad66b5b513acae002c36628529526ddfde
Denq <deniskaa106@gmail.com>
2023-02-22 20:06:38 +0200

    Revert "from character import Character"
    This reverts commit 0f6f1a2d378a04ccc2a3d66e87b74024301f0b35.

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-from character import Character
-
-player1 = Character(name='Vasya', damage=3)
-player2 = Character(name='Petya', health=150)
-
-print(f' - Player 1 - \n{player1}')
-print(f' - Player 2 - \n{player2}')
-
-player1.attack(player2)
-# player2.health -= max(player1.damage, 0)
-print('Player 1 attacted Player 2')
-player2.attack(player1)
-print('Player 2 attacted Player 1')
-
-print(f' - Player 1 - \n{player1}')
-print(f' - Player 2 - \n{player2}')
+print("Hello world")
+print("How are you?")
+print("?")
